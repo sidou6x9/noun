2ce321452133e5756ddedf4de3333c5bcfaba477
sidou6x9 <119750201+sidou6x9@users.noreply.github.com>
2024-04-01 20:22:11 +0200

    y

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,174 +1,17 @@
 import { NavLink } from "react-router-dom";
+import HeroFramer from "./HeroFramer";
 export default function Home() {
   return (
     <div className="bg-white">
       <div
-        className="relative isolate px-6 pt-14 lg:px-8"
         style={{
           backgroundImage:
-            "linear-gradient(180deg, #18453f 40%, #1d6648 80%, #ffffff 100%)",
+            "linear-gradient(180deg, rgba(24, 69, 63, 1) 15%, rgba(29, 102, 72, 1) 70%, rgba(255, 255, 255, 0) 100%)",
         }}
       >
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center">
-            <h1 className="text-4xl  font-bold tracking-tight text-gray-50 sm:text-6xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-100">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 ">
-              <a
-                href="#"
-                className="rounded-md bg-amber-500 text-gray-900 px-3.5 py-2.5 text-sm font-semibold  shadow-sm hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-        </div>
+        <HeroFramer />
       </div>
-      <div className="relative max-w-screen-xl p-4 px-4 mx-auto bg-white  dark:bg-gray-800 sm:px-6 lg:px-8 py-26 lg:mt-20">
-        <div className="relative">
-          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
-            <div className="ml-auto lg:col-start-2 lg:max-w-2xl">
-              <p className="text-base font-semibold leading-6 text-indigo-500 uppercase">
-                Interactive Collaboration
-              </p>
-              <h4 className="mt-2 text-2xl font-extrabold leading-8 text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
-                Foster Team Collaboration for Success
-              </h4>
-              <p className="mt-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
-                Build a simple yet powerful collaborative space for your team.
-                Track progress, share insights, and measure performance. With
-                full control, efficiency has never been easier.
-              </p>
-              <ul className="gap-6 mt-8 md:grid md:grid-cols-2">
-                <li className="mt-6 lg:mt-0">
-                  <div className="flex">
-                    <span className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-green-800 bg-green-100 rounded-full dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        className="w-4 h-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-base font-medium leading-6 text-gray-500 dark:text-gray-200">
-                      Real-time Collaboration
-                    </span>
-                  </div>
-                </li>
-                <li className="mt-6 lg:mt-0">
-                  <div className="flex">
-                    <span className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-green-800 bg-green-100 rounded-full dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        className="w-4 h-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-base font-medium leading-6 text-gray-500 dark:text-gray-200">
-                      Performance Tracking
-                    </span>
-                  </div>
-                </li>
-                <li className="mt-6 lg:mt-0">
-                  <div className="flex">
-                    <span className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-green-800 bg-green-100 rounded-full dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        className="w-4 h-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-base font-medium leading-6 text-gray-500 dark:text-gray-200">
-                      24/7 Support
-                    </span>
-                  </div>
-                </li>
-                <li className="mt-6 lg:mt-0">
-                  <div className="flex">
-                    <span className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-green-800 bg-green-100 rounded-full dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        className="w-4 h-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-base font-medium leading-6 text-gray-500 dark:text-gray-200">
-                      Expert Tips for Efficiency
-                    </span>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div className="relative mt-10 lg:-mx-4 relative-20 lg:mt-0 lg:col-start-1">
-              <div className="relative space-y-4">
-                <div className="flex items-end justify-center space-x-4 lg:justify-start">
-                  <img
-                    className="w-32 rounded-lg shadow-lg md:w-56"
-                    width="200"
-                    src="/female-designing.jpg"
-                    alt="female-designing1"
-                  />
-                  <img
-                    className="w-40 rounded-lg shadow-lg md:w-64"
-                    width="260"
-                    src="/problem-solving.jpg"
-                    alt="problem-solving"
-                  />
-                </div>
-                <div className="flex items-start justify-center ml-12 space-x-4 lg:justify-start">
-                  <img
-                    className="w-24 rounded-lg shadow-lg md:w-40"
-                    width="170"
-                    src="/textures.jpg"
-                    alt="textures"
-                  />
-                  <img
-                    className="w-32 rounded-lg shadow-lg md:w-56"
-                    width="200"
-                    src="/officeWork.jpg"
-                    alt="officeWork"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
       <div className="container p-6 px-6 mx-auto bg-white dark:bg-gray-800 lg:mt-20 lg:mb-20">
         <div className="mb-16 text-center">
           <h2 className="text-base font-semibold tracking-wide text-indigo-600 uppercase">
